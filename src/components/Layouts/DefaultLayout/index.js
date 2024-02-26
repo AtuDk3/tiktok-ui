abcf44a99855a764cb7451396123cb06a2cb4ef1
ATU <thanhtudzx@gmail.com>
2024-02-26 22:25:53 +0700

    26/02/2024

---
+++ src/components/Layouts/DefaultLayout/index.js
@@ -0,0 +1,28 @@
+import Header from "~/components/Layouts/components/Header";
+import SideBar from "./SideBar";
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles);
+
+function DefaultLayout({ children }) {
+  return (
+    <div>
+
+        <div className={cx('wrapper')}>
+        <Header />
+
+            <div className={cx('container')}>
+              <SideBar />
+
+              <div className={cx('content')}>{children}</div>
+            </div>
+            
+
+        </div>
+
+    </div>
+  );
+}
+
+export default DefaultLayout;
